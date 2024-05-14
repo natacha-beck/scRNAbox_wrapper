9096438923b17e6fbc1227c0bbe33dc4b87389bd
natacha-beck <natacha.beck@mcgill.ca>
2024-05-14 07:08:11 -0400

    [ADD] R install file

---
+++ install_packages_scrnabox.R
@@ -0,0 +1,226 @@
+#!/usr/bin/env Rscript
+
+args = commandArgs(trailingOnly=TRUE)
+r_lib_path=args[1]
+## Specify the path 
+.libPaths(r_lib_path) 
+## Where are my packages saved?
+# .libPaths()
+# R package requirements ----------------------------------------------------
+#  if you are using the unix, make sure you already installed: build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev
+
+#########################
+## Install Matrix      ##
+#########################
+write("Installation of packages Matrix", stdout())
+
+install.packages("Matrix", version = "1.6.3", repos='https://cran.r-project.org', dependencies=TRUE)
+if (! library("Matrix", character.only = TRUE, logical.return = TRUE)) {
+  write(paste0("Installation of package ", "Matrix" ," exited with non-zero exit status"),
+         stdout())
+  quit(status = 1, save = "no")
+}
+
+pkgs_cran<-c('jquerylib', 'sass', 'fontawesome', 'bslib', 'rmarkdown')
+
+for (pkg in (pkgs_cran)) {
+  install.packages(pkg, repos='https://cran.r-project.org', dependencies=TRUE)
+  if (! library(pkg, character.only = TRUE, logical.return = TRUE)) {
+    write(paste0("Installation of package ", pkg ," exited with non-zero exit status"),
+          stdout())
+    quit(status = 1, save = "no")
+  }
+}
+
+#########################
+## Install BiocManager ##
+#########################
+write("Installation of BiocManager", stdout())
+
+install.packages("BiocManager",repos='https://cran.r-project.org', dependencies=TRUE)
+
+if (! library("BiocManager", character.only = TRUE, logical.return = TRUE)) {
+  write(paste0("Installation of package ", "BiocManager" ," exited with non-zero exit status"),
+        stdout())
+  quit(status = 1, save = "no")
+}
+
+######################
+## Install multtest ##
+######################
+write("Installation of multtest", stdout())
+
+BiocManager::install("multtest")
+
+if (! library("multtest", character.only = TRUE, logical.return = TRUE)) {
+  write(paste0("Installation of package ", "multtest" ," exited with non-zero exit status"),
+        stdout())
+  quit(status = 1, save = "no")
+}
+
+######################
+## Install via CRAN ##
+######################
+write("Installation of packages via CRAN", stdout())
+
+pkgs_cran<-c('beeswarm','pdftools','magick','callr','pkgbuild','diffobj','reprex','rvest','RSpectra','uwot',
+	     'broom', 'dbplyr', 'modelr', 'tidyr',
+             'tidyverse', 'foreach', 'doParallel', 'Seurat', 'caTools','gplots','ROCR','vctrs',
+             'fossil', 'openxlsx', 'stringr','ggsignif','ggpubr','data.table','ggrepel', 'scCustomize',
+             'fs', 'bslib', 'pkgload', 'shiny', 'gargle', 'htmlwidgets', 'testthat',
+             'usethis', 'miniUI', 'pkgdown', 'profvis', 'roxygen2', 'mutoss', 'RcppGSL', 'RcppZiggurat',
+             'igraph', 'Rfast','leiden','reticulate')
+
+for (pkg in (pkgs_cran)) {
+  install.packages(pkg, repos='https://cran.r-project.org', dependencies=TRUE)
+  if (! library(pkg, character.only = TRUE, logical.return = TRUE)) {
+    write(paste0("Installation of package ", pkg ," exited with non-zero exit status"),
+          stdout())
+    quit(status = 1, save = "no")
+  }
+}
+
+######################
+## Installl devtool ##
+######################
+install.packages("devtools", repos='https://cran.r-project.org', dependencies=TRUE)
+if (! library("devtools", character.only = TRUE, logical.return = TRUE)) {
+    write(paste0("Installation of package ", "devtools" ," exited with non-zero exit status"),stdout())
+}
+
+
+###########################
+## Install DoubletFinder ##
+###########################
+write("Installation of DoubletFinder", stdout())
+
+remotes::install_github('chris-mcginnis-ucsf/DoubletFinder', force = TRUE)
+
+if (! library("DoubletFinder", character.only = TRUE, logical.return = TRUE)) {
+  write(paste0("Installation of package ", "DoubletFinder" ," exited with non-zero exit status"),stdout())
+}
+
+#############################
+## Install via BiocManager ##
+#############################
+write("Installation of package via BiocManager",  stdout())
+
+pkgs_BiocManager <- c('cowplot','xlsx','clustree','enrichR','stringi','limma', 'edgeR',
+'org.Hs.eg.db', 'SoupX','MatrixGenerics','BiocGenerics','S4Vectors','IRanges','GenomeInfoDb','GenomicRanges', 'Biobase',
+'SummarizedExperiment', 'SingleCellExperiment', 'DropletUtils', 'EnhancedVolcano', 'MAST')
+
+for (pkg in basename(pkgs_BiocManager)) {
+  BiocManager::install(pkg, ask = FALSE, update = FALSE)
+  if (! library(pkg, character.only = TRUE, logical.return = TRUE)) {
+    write(paste0("Installation of package ",pkg," exited with non-zero exit status"),  stdout())
+    quit(status = 1, save = "no")
+  }
+}
+
+#############################
+## Install via BiocManager ##
+#############################
+write("Installation of package via BiocManager",  stdout())
+
+pkgs_BiocManager <- c('cowplot','xlsx','clustree','enrichR','stringi','limma', 'edgeR',
+'org.Hs.eg.db', 'SoupX','MatrixGenerics','BiocGenerics','S4Vectors','IRanges','GenomeInfoDb','GenomicRanges', 'Biobase',
+'SummarizedExperiment', 'SingleCellExperiment', 'DropletUtils', 'EnhancedVolcano', 'MAST')
+
+for (pkg in basename(pkgs_BiocManager)) {
+  BiocManager::install(pkg, ask = FALSE, update = FALSE)
+  if (! library(pkg, character.only = TRUE, logical.return = TRUE)) {
+    write(paste0("Installation of package ",pkg," exited with non-zero exit status"),  stdout())
+    quit(status = 1, save = "no")
+  }
+}
+
+######################
+## Install scrnabox ##
+######################
+write("Installation of package scrnaboxR",  stdout())
+
+devtools::install_github("neurobioinfo/scrnabox/scrnaboxR")
+
+if (! library("scrnaboxR", character.only = TRUE, logical.return = TRUE)) {
+    write(paste0("Installation of package ", "scrnaboxR" ," exited with non-zero exit status"),stdout())
+}
+
+
+#-------------------------------------------------------------------------------------#
+
+
+
+
+## install via Github
+write("Installation of packages via Github",  stdout())
+
+devtools::install_github("igraph/rigraph")
+if (! library("igraph", character.only = TRUE, logical.return = TRUE)) {
+    write(paste0("Installation of package ", "ggplot2" ," exited with non-zero exit status"),stdout())
+}
+
+#devtools::install_github("tidyverse/tidyverse",force = TRUE)
+#if (! library("tidyverse", character.only = TRUE, logical.return = TRUE)) {
+#    write(paste0("Installation of package ", "tidyverse" ," exited with non-zero exit status"),stdout())
+#}
+
+#devtools::install_github('chris-mcginnis-ucsf/DoubletFinder',force = TRUE)
+#if (! library("DoubletFinder", character.only = TRUE, logical.return = TRUE)) {
+#    write(paste0("Installation of package ", "DoubletFinder" ," exited with non-zero exit status"),stdout())
+#}
+
+
+## install via Cran
+write("Installation of packages via cran",  stdout())
+
+# Seurat need modules of gsl, hdf5, gcc
+#pkgs_seurat<-'Seurat'
+#install.packages(pkg, repos='https://cran.r-project.org', dependencies=TRUE)
+
+
+#if (! library(pkgs_seurat, character.only = TRUE, logical.return = TRUE)) {
+#  write(paste0("Installation of package ", pkgs_seurat ," exited with non-zero exit status, you should install it manually"),
+#        stdout())
+#  quit(status = 1, save = "no")
+#}
+
+
+
+pkgs_cran<-c('tidyverse', 'foreach', 'doParallel', 'Seurat', 'caTools','gplots','ROCR','vctrs', 'fossil', 'openxlsx',
+'stringr','ggpubr','data.table','ggrepel', 'scCustomize')
+
+for (pkg in (pkgs_cran)) {
+  install.packages(pkg, repos='https://cran.r-project.org', dependencies=TRUE)
+  if (! library(pkg, character.only = TRUE, logical.return = TRUE)) {
+    write(paste0("Installation of package ", pkg ," exited with non-zero exit status"),
+          stdout())
+    quit(status = 1, save = "no")
+  }
+}
+
+
+## install Bioconductor 
+if (!require("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager",repos='https://cran.r-project.org', dependencies=TRUE)
+}
+
+pkgs_BiocManager <- c('Matrix','cowplot','xlsx','clustree','enrichR','stringi','limma', 'edgeR', 
+'org.Hs.eg.db', 'SoupX','MatrixGenerics','BiocGenerics','S4Vectors','IRanges','GenomeInfoDb','GenomicRanges', 'Biobase', 
+'SummarizedExperiment', 'SingleCellExperiment', 'DropletUtils', 'EnhancedVolcano', 'MAST')
+
+## install via BiocManager
+write("Installation of package via BiocManager",  stdout())
+for (pkg in basename(pkgs_BiocManager)) {
+  BiocManager::install(pkg, ask = FALSE, update = FALSE)
+  if (! library(pkg, character.only = TRUE, logical.return = TRUE)) {
+    write(paste0("Installation of package ",pkg," exited with non-zero exit status"),  stdout())
+    quit(status = 1, save = "no")
+  }
+}
+
+## Install scrnabox
+devtools::install_github("neurobioinfo/scrnabox/scrnaboxR")
+
+if (! library("scrnaboxR", character.only = TRUE, logical.return = TRUE)) {
+    write(paste0("Installation of package ", "scrnaboxR" ," exited with non-zero exit status"),stdout())
+}
